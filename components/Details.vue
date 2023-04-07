6675f6e1b3e058ca472c463b8b77a44c195271aa
avd <anavaleriaduguleanu@gmail.com>
2023-04-07 15:05:12 +0300

    showed authors' names

--- components/Details.vue
+++ components/Details.vue
@@ -4,10 +4,41 @@
     {{ item.title }}
   </h2>
   <p>
+    {{ author.name }}
+  </p>
+  <p>
+    {{ item.userId }}
+  </p>
+  <p>
     {{ item.body }}
   </p>
 </template>
 
 <script setup>
+import { useNews } from "~/composables/useNews";
+
 const props = defineProps({ item: Object });
+const author = ref({});
+const useAuthor = async (userId) => {
+  const result = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  const author = await result.json();
+  return author;
+};
+watch(
+  () => {
+    console.log("checking", props.item.userId);
+    return props.item.userId;
+  },
+  () => {
+    console.log("refreshing author", props.item.userId);
+    useAuthor(props.item.userId).then((_author) => {
+      author.value = _author;
+    });
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
